8b2e8ff256eb3458e74246316e54eca43e4f23b7
Jonas Dahl <jonas@jdahl.se>
2014-04-29 17:56:22 +0200

    ScreenSize

--- my-gdx-game-desktop/src/com/me/mygdxgame/Main.java
+++ my-gdx-game-desktop/src/com/me/mygdxgame/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "€ggspelet";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 384; // S4: 1920*1280
+		cfg.height = 256;
 		
 		new LwjglApplication(new MyGdxGame(), cfg);
 	}
