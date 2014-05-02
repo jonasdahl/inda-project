5fc6e6ff1887be3561d7cb5d9bcc03f5ccd392bd
Jonas Dahl <jonas@jdahl.se>
2014-05-02 23:05:25 +0200

    Egg start

--- egg-pillow/src/com/eggpillow/Pillow.java
+++ egg-pillow/src/com/eggpillow/Pillow.java
@@ -30,13 +30,12 @@ public class Pillow extends Sprite {
 
 	@Override
 	public void draw(SpriteBatch batch) {
-
 		super.draw(batch);
 	}
 	
-	
 	/**
 	 * Checks if testX and testY is inside or very close to the pillow.
+	 * @return true if testX and textY is close to the pillow
 	 */
 	public boolean inside(int testX, int testY) {
 		if (testX > getX() - paddingX
@@ -46,6 +45,5 @@ public class Pillow extends Sprite {
 			return true;
 		}
 		return false;
-
 	}
 }
